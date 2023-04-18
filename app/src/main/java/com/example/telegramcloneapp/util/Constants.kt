c74890ce912968a7a425665329120f0fcc888b4a
samruddhi <samruddhikashmire19@gmail.com>
2023-04-18 09:06:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/telegramcloneapp/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.telegramcloneapp.util
+
+object  Constants {
+    const val  MIN_USER_NAME_LEN = 3
+}
---
+++ app/src/test/java/dagger/hilt/internal/aggregatedroot/codegen/_com_example_telegramcloneapp_ChatApplicationTest.java
@@ -0,0 +1,8 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import junit.framework.TestCase;
+
+public class _com_example_telegramcloneapp_ChatApplicationTest extends TestCase {
+
+
+}
